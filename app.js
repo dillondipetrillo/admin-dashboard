ef7843cd4f9b55033bd7b09cc6d60fbd124f4e4f
Dillon Dipetrillo <Dillon.Dipetrillo@surepayroll.com>
2023-03-16 19:14:40 -0400

    added finishing touches on everything and mobile responsive

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const sidebarNavLink = document.querySelectorAll(".sidebar__nav-link");
 const sidebarHelpLink = document.querySelectorAll(".sidebar__help-link");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.querySelector(".dark-overlay");
+const burgerMenu = document.querySelector(".header__top-left-menu");
+const closeBtn = document.querySelector(".close-btn");
 
 const sidebarLinks = [...sidebarNavLink, ...sidebarHelpLink];
 
+// Adds active class to selected sidebar item
 const addActiveClass = (e) => {
   if (e.target.classList.contains("active")) return;
   if (!e.target.classList.contains("active")) {
@@ -15,4 +20,28 @@ const addActiveClass = (e) => {
   }
 };
 
+// Opens sidebar on smaller devices
+const openSidebar = () => {
+  if (!sidebar.classList.contains("open")) {
+    sidebar.classList.add("open");
+    overlay.classList.add("open");
+    document.body.classList.add("no-scroll");
+  }
+};
+
+// Closes sidebar if clicked outside of opened sidebar or close button
+const closeSidebar = () => {
+  if (sidebar.classList.contains("open")) {
+    sidebar.classList.remove("open");
+    overlay.classList.remove("open");
+    document.body.classList.remove("no-scroll");
+  }
+};
+
+overlay.addEventListener("click", closeSidebar);
+
+closeBtn.addEventListener("click", closeSidebar);
+
 sidebarLinks.forEach((link) => link.addEventListener("click", addActiveClass));
+
+burgerMenu.addEventListener("click", openSidebar);
